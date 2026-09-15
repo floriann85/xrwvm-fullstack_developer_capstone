91afcd8c9d1dc4a6d48a8e26eefb18e9701223df
FlorianN85 <fl.nolte@googlemail.com>
2026-09-15 14:03:40 -0400

    Fixes: Path settings urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
+from django.http import HttpResponse
+
+def manifest(request):
+    return HttpResponse("{}", content_type="application/json")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,13 +30,19 @@ urlpatterns = [
     # Django API
     path('djangoapp/', include('djangoapp.urls')),
 
+    # manifest.json separate handling
+    path('manifest.json', manifest),
+
     # React SPA    
     path('', TemplateView.as_view(template_name="Home.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),    
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),    
+    path('postreview/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+
+    # SPA fallback
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
