1c735749646dbe3116710bfaed07cfb104088a75
floriann85 <64438712+floriann85@users.noreply.github.com>
2026-09-16 22:28:15 +0200

    Update models.py
    Fix flake8 issues in models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-import datetime
 
 # Create your models here.
 
