1555b78b19ed3e62d58d292b19cec57dd8ec492b
floriann85 <64438712+floriann85@users.noreply.github.com>
2026-09-16 22:02:23 +0200

    Update 0001_initial.py
    Fix flake8 issues in 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
             ],
@@ -24,12 +32,47 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('HATCHBACK', 'Hatchback'), ('COUPE', 'Coupe')], default='SUV', max_length=20)),
+                (
+                    'type',
+                    models.CharField(
+                        choices=[
+                            ('SEDAN', 'Sedan'),
+                            ('SUV', 'SUV'),
+                            ('WAGON', 'Wagon'),
+                            ('HATCHBACK', 'Hatchback'),
+                            ('COUPE', 'Coupe'),
+                        ],
+                        default='SUV',
+                        max_length=20
+                    )
+                ),
                 ('dealer_id', models.IntegerField()),
-                ('year', models.IntegerField(validators=[django.core.validators.MinValueValidator(1900), django.core.validators.MaxValueValidator(2026)])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                (
+                    'year',
+                    models.IntegerField(
+                        validators=[
+                            django.core.validators.MinValueValidator(1900),
+                            django.core.validators.MaxValueValidator(2026),
+                        ]
+                    )
+                ),
+                (
+                    'car_make',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to='djangoapp.carmake'
+                    )
+                ),
             ],
         ),
     ]
