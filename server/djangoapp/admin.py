101f97302e9c2b7364d122c64fc6ff71a7970b36
floriann85 <64438712+floriann85@users.noreply.github.com>
2026-09-16 21:47:33 +0200

    Update admin.py
    Fix flake8 issues in admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # Inline class to show CarModels inside CarMake
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1  # number of empty rows shown
+
 
 # Admin class for CarModel
 class CarModelAdmin(admin.ModelAdmin):
@@ -12,11 +14,13 @@ class CarModelAdmin(admin.ModelAdmin):
     list_filter = ('type', 'year', 'car_make')
     search_fields = ('name',)
 
+
 # Admin class for CarMake
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
     inlines = [CarModelInline]
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
