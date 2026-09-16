538069b96e975f19e6175254e0958224536a1a4f
floriann85 <64438712+floriann85@users.noreply.github.com>
2026-09-16 22:21:53 +0200

    Update urls.py
    Fix flake8 issues in urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,12 +5,17 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
     # path for registration
     path('register', views.registration, name='register'),
-    
+
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path(
+        route='login',
+        view=views.login_user,
+        name='login'
+    ),
 
     # path for logout
     path('logout', views.logout_request, name='logout'),
@@ -19,13 +24,31 @@ urlpatterns = [
     path('get_cars/', views.get_cars, name='getcars'),
 
     # path for dealer reviews view
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    path(
+        route='get_dealers/',
+        view=views.get_dealerships,
+        name='get_dealers'
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>/',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
 
     # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>/', view=views.get_dealer_reviews, name='dealer_reviews'),
-    path(route='add_review', view=views.add_review, name='add_review'),
-
-
+    path(
+        route='reviews/dealer/<int:dealer_id>/',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
+    path(
+        route='add_review',
+        view=views.add_review,
+        name='add_review'
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
